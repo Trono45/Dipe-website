e00a388ef58659596b601aada7871657bf77aba0
Trono45 <stesiegelp2000@gmail.com>
2022-11-14 09:35:35 -0500

    init

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 //Database credentials. Assuming you are running MySQL
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'epiz_32993252');
-define('DB_PASSWORD', '3CF9Evg2Cj');
-define('DB_NAME', 'epiz_32993252_users');
+define('DB_SERVER', 'us-cdbr-east-06.cleardb.net');
+define('DB_USERNAME', 'b0c5854bb14676');
+define('DB_PASSWORD', '4907c512');
+define('DB_NAME', 'heroku_034a6cfa247a56b');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
