7ef4cb200851bbc8191c03edaa04075654b0eaca
Trono45 <stesiegelp2000@gmail.com>
2022-11-14 22:51:13 -0500

    init

--- register.php
+++ register.php
@@ -196,10 +196,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+        console.log("paso");
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
+        console.log($username);
+        console.log($password);
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
