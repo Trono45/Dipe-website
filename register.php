5298e057ecd5990d01d9486f35e446109355a19f
Trono45 <stesiegelp2000@gmail.com>
2022-11-14 22:16:22 -0500

    init

--- register.php
+++ register.php
@@ -127,39 +127,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
     
-    // // Ingresar genero
-    // if(empty(trim($_POST["gender"]))){
-    //     $gender_err = "Por favor ingrese el genero";
-    // } elseif(!preg_match('/^[a-zA-Z]+$/', trim($_POST["gender"]))){
-    //     $gender_err = "Los nombres solo deben incluir letras.";
-    // } else{
-    //     // Prepare a select statement
-    //     $sql = "SELECT id FROM users WHERE gender = ?";
+    // Ingresar genero
+    if(empty(trim($_POST["gender"]))){
+        $gender_err = "Por favor ingrese el genero";
+    } elseif(!preg_match('/^[a-zA-Z]+$/', trim($_POST["gender"]))){
+        $gender_err = "Los nombres solo deben incluir letras.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE gender = ?";
         
-    //     if($stmt = mysqli_prepare($link, $sql)){
-    //         // Bind variables to the prepared statement as parameters
-    //         mysqli_stmt_bind_param($stmt, "s", $param_gender);
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_gender);
             
-    //         // Set parameters
-    //         $param_gender = trim($_POST["gender"]);
+            // Set parameters
+            $param_gender = trim($_POST["gender"]);
             
-    //         // Attempt to execute the prepared statement
-    //         if(mysqli_stmt_execute($stmt)){
-    //             /* store result */
-    //             mysqli_stmt_store_result($stmt);
-    //             if(mysqli_stmt_num_rows($stmt) == 1){
-    //                 $gender_err = "Seleccione M - para Masculino y F - para Femenino";
-    //             } else{
-    //                $gende = trim($_POST["gender"]);
-    //            }
-    //         } else{
-    //             $error =  "Error intente luego.";
-    //         }
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $gender_err = "Seleccione M - para Masculino y F - para Femenino";
+                } else{
+                   $gende = trim($_POST["gender"]);
+               }
+            } else{
+                $error =  "Error intente luego.";
+            }
 
-    //         // Close statement
-    //         mysqli_stmt_close($stmt);
-    //     }
-    // }
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
 
     // // Ingresar un email
     // if(empty(trim($_POST["email"]))){
