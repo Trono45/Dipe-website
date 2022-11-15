a3677413169ffc95445bddea3b0dac29cedb10bd
Trono45 <stesiegelp2000@gmail.com>
2022-11-15 00:12:30 -0500

    s

--- register.php
+++ register.php
@@ -209,7 +209,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             $param_name = $name;
-            echo "<script>console.log('Debug Objects: " . $name . "' );</script>";
+            echo "<script>console.log('{$name}' );</script>";
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
