c5454d05e749b3edd410d59d7cd9155bff379e04
Trono45 <stesiegelp2000@gmail.com>
2022-11-14 22:57:14 -0500

    INIT

--- register.php
+++ register.php
@@ -66,7 +66,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+        echo "<script>console.log('{SIRVE}' );</script>";
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
          
