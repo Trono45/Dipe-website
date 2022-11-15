63ad61fe7bdb7d1ac8691cde4616ff1fc856f743
Trono45 <stesiegelp2000@gmail.com>
2022-11-14 22:36:35 -0500

    init

--- register.php
+++ register.php
@@ -196,22 +196,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($name_err) && empty($age_err) && empty($gender_err) && empty($email_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password, name, age, gender, email) VALUES (?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password,$param_username. $param_username, $param_username, $param_username);
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
             
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $param_name = $username;
-            $param_age = $username;
-            $param_gender = $username;
-            $param_email = $username;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -220,12 +216,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             } else{
                 $error = "error, por favor intente más tarde.";
             }
+
             // Close statement
             mysqli_stmt_close($stmt);
         }
     }
-    // Close connection
-    mysqli_close($link);
+
     // Close connection
     mysqli_close($link);
 }
